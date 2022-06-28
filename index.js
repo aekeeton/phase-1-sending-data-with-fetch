bd7acc7029efa32e5258f2db4cd2506c29e59e04
aekeeton <ashley.keeton@outlook.com>
2022-06-28 16:27:42 -0500

    commit tree 2fab606e0360dc8dc92d3da4816d32c4ab822e68

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your code here
+function submitData(firstName, email) {
+    const configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json"
+        },
+        body: JSON.stringify({
+            "name": firstName,
+            "email": email
+        })
+    }
+    return fetch("http://localhost:3000/users", configObj)
+    .then(response => response.json())
+    .then(object => document.body.innerHTML = object.id)
+    .catch(error => document.write(error.message))
+}
+console.log(error)
+
+
+function addNewContact (newID) {
+    document.getElementsByTagName("body")[0].innerHTML = newID
+    document.body.append(message)
+}
+submitData("name", "name@name.com")
